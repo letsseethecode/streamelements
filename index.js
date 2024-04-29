3839ebddef9b0125b4fa9b745200fbddf0fcc74f
Chris Kemp <chris.kemp3000@gmail.com>
2024-04-29 20:40:22 +0100

    Improvements on stream

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-// var elements = document.getElementsByClassName('highlight');
-// for (var e of elements) {
-//     highlight(e);
-// }
-var elements = document.getElementsByClassName('console');
-for (var e of elements) {
-    console(e);
-}
+(function () {
+    var elements = document.getElementsByClassName('console');
+    for (var e of elements) {
+        console(e);
+    }
 
-function highlight(e) {
-    let s = e.innerHTML
-        .split('')
-        .map(letter => `<span class="animated-letter wobble">${letter}</span>`)
-        .join('');
-    e.innerHTML = s;
-}
-
-function console(e) {
-    let i = 0;
-    s = (e.innerHTML + ' ')
-        .split('')
-        .map(c => `<span class="console__character" style="animation-delay: ${i += 100}ms">${c.replace(' ', '&nbsp;')}</span>`)
-        .join('');
-    e.innerHTML = s;
-}
+    function console(e) {
+        s = (e.innerHTML + ' ')
+            .split('')
+            .map((c, i) => `<span class="console__character" style="animation-delay: ${i * 100}ms">${c.replace(' ', '&nbsp;')}</span>`)
+            .join('');
+        e.innerHTML = s;
+    }
+})();
